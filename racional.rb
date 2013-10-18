f973c84906eb820312e28923fd721981c8bdb4a9
Juan Lucas <alu0100499452@ull.edu.es>
2013-10-18 14:43:51 +0100

    Hecho el initialize

--- racional.rb
+++ racional.rb
@@ -3,4 +3,12 @@
 require "./gcd.rb"
 
 class Fraccion
+   attr_reader :a, :b # se usa para tener un metodo de lectura de a y b.
+
+   # a es numerador y b denominador
+
+   def initialize (numerador, denominador)
+    @a, @b = numerador, denominador
+  end
+
 end
