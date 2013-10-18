8d742e695d4804704b06ff927de46d4d1805f46d
Juan Lucas <alu0100499452@ull.edu.es>
2013-10-18 15:01:19 +0100

    Hecho test initialize y to_s

--- tc_racional.rb
+++ tc_racional.rb
@@ -12,6 +12,12 @@ class Test_Fraccion < Test::Unit::TestCase
     @f2 = Fraccion.new(10, 8)
   end
 
+  #Test del inicialice y del to_s
+
+  def test_initialize
+    assert_equal("5/10", Fraccion.new(5, 10).to_s)
+    assert_equal("10/8", @f2.to_s)
+  end
 
 
 end
